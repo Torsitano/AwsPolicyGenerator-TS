512def54b8a238d83b3f6723a77213fa56975381
Shawn Torsitano <shawn.torsitano@gmail.com>
2023-05-25 08:37:14 -0400

    updates

--- test/statementTest.ts
+++ test/statementTest.ts
@@ -1,26 +1,35 @@
+import { PrivilegeLevel } from '../src/awsPolicyGenerator/interfaces/interfaces'
 import { Statement, Effect } from './../src/awsPolicyGenerator/statementComponents/Statement'
 
+//@ts-ignore
+const privLevels: PrivilegeLevel[] = [ 'readPrivileges', 'listPrivileges', 'tagPrivileges', 'writePrivileges', 'permManPrivileges' ]
 
+const statement = new Statement( Effect.ALLOW, true ).addActionsForResource( {
+    service: 'ec2',
+    resource: 'instance',
+    privLevels: privLevels
+} )
 
-const statement = new Statement( Effect.ALLOW, true )
-    // .addActionsForResource( {
-    //     service: 'iam',
-    //     resource: 'role',
-    //     privLevels: [ 'readPrivileges', 'listPrivileges' ],
-    //     resourceArn: 'arn:aws:iam::123456789123:role/myiamrole'
-    // } )
-    .addActions( [ 'ec2:RunInstances' ] )
+// .addActionsForResource( {
+//     service: 'ec2',
+//     resource: 'vpc',
+//     privLevels: privLevels
+// } )
+// .addActionsForResource( {
+//     service: 'ec2',
+//     resource: 'subnet',
+//     privLevels: privLevels
+// } )
+
+const yamlOutput = statement.toYaml()
+
+console.log( yamlOutput )
 
 // const statement = new Statement( 'Allow' )
 //     .addActions( [ 's3:CreateBucket', 's3:ListAllMyBuckets' ] )
 
 
 //console.log( statement.resources )
-
-//@ts-ignore
-const yamlOutput = statement.toYaml()
-
-console.log( yamlOutput )
 
 //console.log( statement.toYaml() )
 
